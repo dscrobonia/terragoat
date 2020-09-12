088f58e1d81b7251fa1e9a9189869bb06c6b65ee
David Scrobonia <davidscrobonia@gmail.com>
2020-09-12 16:36:03 -0700

    moar issues

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -36,3 +36,18 @@ resource "aws_lambda_function" "analysis_lambda" {
     }
   }
 }
+
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
