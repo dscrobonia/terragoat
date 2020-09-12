4ed195b1c77bb89a9777734ff1ed59ed911983fe
David Scrobonia <davidscrobonia@gmail.com>
2020-09-12 16:42:17 -0700

    add things

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -83,3 +83,19 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = "${aws_eks_cluster.eks_cluster.certificate_authority.0.data}"
 }
+
+
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
