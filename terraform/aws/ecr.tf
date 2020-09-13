7b841911404c7c6a9a246033221096b3bf8f8cd9
David Scrobonia <davidscrobonia@gmail.com>
2020-09-12 17:01:33 -0700

    bad stuff

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -23,3 +23,38 @@ resource null_resource "push_image" {
     BASH
   }
 }
+
+
+
+
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+
+
+
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
