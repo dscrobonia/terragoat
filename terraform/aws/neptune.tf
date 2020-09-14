30a012e95ec893682d21fe3f960833ed10c7e342
David Scrobonia <davidscrobonia@gmail.com>
2020-09-14 13:52:52 -0700

    fix some add one

--- terraform/aws/neptune.tf
+++ terraform/aws/neptune.tf
@@ -22,3 +22,18 @@ resource "aws_neptune_cluster_snapshot" "default" {
   db_cluster_identifier               = aws_neptune_cluster.default.id
   db_cluster_snapshot_identifier      = "resourcetestsnapshot1"
 }
+
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
