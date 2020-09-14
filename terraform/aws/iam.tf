e849718f4554823b33b20c53b19960722165ffd1
David Scrobonia <davidscrobonia@gmail.com>
2020-09-14 12:54:05 -0700

    fix an issue

--- terraform/aws/iam.tf
+++ terraform/aws/iam.tf
@@ -43,20 +43,3 @@ output "username" {
 output "secret" {
   value = aws_iam_access_key.user.encrypted_secret
 }
-
-
-
-resource "aws_s3_bucket" "data" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
-
