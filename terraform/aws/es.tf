8c1f5de79d33835776efe6edbe55e0a4769eded6
David Scrobonia <davidscrobonia@gmail.com>
2020-09-14 12:55:40 -0700

    more fixes

--- terraform/aws/es.tf
+++ terraform/aws/es.tf
@@ -31,33 +31,3 @@ resource "aws_elasticsearch_domain_policy" "monitoring-framework-policy" {
   domain_name     = aws_elasticsearch_domain.monitoring-framework.domain_name
   access_policies = data.aws_iam_policy_document.policy.json
 }
-
-
-
-resource "aws_s3_bucket" "data" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
-
-resource "aws_s3_bucket" "data" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
