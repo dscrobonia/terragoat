e9cb715a2ae33864a1475ebb58ee0e88b13e1115
David Scrobonia <davidscrobonia@gmail.com>
2020-09-14 11:57:39 -0700

    newer action

--- terraform/aws/iam.tf
+++ terraform/aws/iam.tf
@@ -44,3 +44,19 @@ output "secret" {
   value = aws_iam_access_key.user.encrypted_secret
 }
 
+
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+
