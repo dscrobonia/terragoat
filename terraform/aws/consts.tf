3a24eb1a95d89eff71a3a0296845887bbeb8f8f3
David Scrobonia <davidscrobonia@gmail.com>
2020-09-14 13:29:21 -0700

    testing new

--- terraform/aws/consts.tf
+++ terraform/aws/consts.tf
@@ -58,3 +58,17 @@ variable "neptune-dbname" {
   description = "Name of the Neptune graph database"
   default     = "neptunedb1"
 }
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
