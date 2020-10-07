8f1fc38ae678ee2f13aaad020c4b0a4a970c66fd
David Scrobonia <davidscrobonia@gmail.com>
2020-10-07 11:06:07 -0700

    like 4 issues

--- terraform/aws/bigbad.tf
+++ terraform/aws/bigbad.tf
@@ -11,6 +11,7 @@ resource "aws_s3_bucket" "data" {
     Environment = local.resource_prefix.value
   }
 }
+
 resource aws_ecr_repository "repository" {
   name                 = "${local.resource_prefix.value}-repository"
   image_tag_mutability = "MUTABLE"
@@ -24,3 +25,42 @@ locals {
   docker_image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${aws_ecr_repository.repository.name}"
 }
 
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
--- terraform/aws/neptune.tf
+++ terraform/aws/neptune.tf
@@ -37,3 +37,19 @@ resource "aws_s3_bucket" "data" {
     Environment = local.resource_prefix.value
   }
 }
+
+
+resource "aws_s3_bucket" "somethingelse" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket         = "${local.resource_prefix.value}-data"
+  acl            = "public-read"
+  force_destroy  = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+
