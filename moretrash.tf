bb0d99a9da1034d0ad129b0307156b86441d10ac
David Scrobonia <dscrobonia@users.noreply.github.com>
2021-04-14 18:27:39 -0700

    Update moretrash.tf

--- moretrash.tf
+++ moretrash.tf
@@ -27,3 +27,19 @@ resource "aws_s3_bucket" "data" {
     Environment = local.resource_prefix.value
   }
 }
+
+
+
+resource "aws_s3_bucket" "daasdfta" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logsasdf
+  # bucket does not have versioning asdfasasdfasdfasdf
+  bucket        = "new--asdf-bucket"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
