0bf6bfba93d91814629147c4cbe329bc123f428f
David Scrobonia <dscrobonia@users.noreply.github.com>
2021-04-14 18:45:06 -0700

    Update mynewtrash.tf

--- mynewtrash.tf
+++ mynewtrash.tf
@@ -4,7 +4,19 @@ resource "aws_s3_bucket" "new-trash-bucket" {
   # dd does not have access logsasdf
   # bucket does not have 
   bucket        = "new-trash-bucket"
-  acl           = "public-read"
+  acl           = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
